a526ae5df9fb1b133d267c037bab621a2017f712
NaosaikaDevelopment <naosaikadevelopment@gmail.com>
2025-09-10 05:28:50 +0900

    Update RunAI.py

--- project-LAPAI/RunAI.py
+++ project-LAPAI/RunAI.py
@@ -8,7 +8,7 @@ clss = ["lemonade-server","status"]
 hasil = subprocess.run(clss, capture_output=True, text=True, shell=True)
 if "Server is not running" in hasil.stdout:
     print("error; server Offline, start automatic!")
-    asls = r"D:\ND\bin\lemonade_server.vbs"
+    asls = r"...\lemonade-serverLocation\lemonade_server.vbs"
     subprocess.run(asls, shell=True)
     LAPAI_Core.Tm()
 else:

e0e97cd02c5a6c0794282381c09a1a87780ab484
NaosaikaDevelopment <naosaikadevelopment@gmail.com>
2025-09-12 19:48:56 +0900

    deleteUpdate

--- project-LAPAI/Auto_installing_Env.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo [INFO] starting Installing dependencies from requirements.txt...
-echo [ATTENTION] make sure you are connected to internet!
-
-REM Install requirements
-pip install -r requirements.txt
-
-echo [INFO] Installing Done.
-pause
